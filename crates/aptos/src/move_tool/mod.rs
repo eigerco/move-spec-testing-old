d345df9af1d456c8cf0b3e89e06bc87e6ffa23b3
Piotr Olszewski <asmie@asmie.pl>
2024-01-22 13:40:31 +0100

    [move-mutator][move-spec-test] rename `Options` to `CLIOptions`

--- crates/aptos/src/move_tool/mod.rs
+++ crates/aptos/src/move_tool/mod.rs
@@ -529,7 +529,7 @@ pub struct MutatePackage {
     move_options: MovePackageDir,
     /// Options specific for the mutator tool
     #[clap(flatten)]
-    mutator_options: Option<move_mutator::cli::Options>,
+    mutator_options: Option<move_mutator::cli::CLIOptions>,
 }
 
 #[async_trait]
@@ -584,7 +584,7 @@ pub struct SpecTestPackage {
     move_options: MovePackageDir,
     /// Options specific for the spec-test tool
     #[clap(flatten)]
-    spec_test_options: Option<move_spec_test::cli::Options>,
+    spec_test_options: Option<move_spec_test::cli::CLIOptions>,
 }
 
 #[async_trait]
